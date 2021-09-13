b66ddc14db1eac1d0991fe603de36aae9e7131cb
Justin Anthony <justincanthony@gmail.com>
2021-09-13 17:43:11 -0600

    Create route paths

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import { ShowContext, ShowContextProvider } from '../../contexts/ShowContext';
+import { ShowContextProvider } from '../../contexts/ShowContext';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Container from './components/container/Container';
+import Footer from './components/footer/Footer';
 
 function App() {
-  return <ShowContextProvider></ShowContextProvider>;
+  return (
+    <ShowContextProvider>
+      <Switch>
+        <Route
+          exact
+          path="gone-phishin/years"
+          render={() => {
+            <div className="app">
+              <Navbar />
+              <Container type="years" />
+              <Footer />
+            </div>;
+          }}
+        />
+        <Route
+          exact
+          path="gone-phishin/years/:year"
+          render={({ match }) => {
+            return (
+              <div className="app">
+                <Navbar />
+                <Container id={match.params.year} />
+                <Footer />
+              </div>
+            );
+          }}
+        />
+        <Route
+          exact
+          path="gone-phishin/shows/:id"
+          render={({ match }) => {
+            return (
+              <div className="app">
+                <Navbar />
+                <Container id={match.params.id} />
+                <Footer />
+              </div>
+            );
+          }}
+        />
+      </Switch>
+    </ShowContextProvider>
+  );
 }
 
 export default App;
