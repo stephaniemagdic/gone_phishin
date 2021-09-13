fa9b2a83cdc36d7bc1e2a81aaa7c53e243bbeb79
Nina Brissey <nbrissey@gmail.com>
2021-09-13 13:59:07 -0700

    Convert to async/await

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
-const fetchData = (path) => {
+export const fetchData = async (path) => {
   const bearer =
     'Bearer ' +
     '993a1943848ae99388901820d93b556d71c8e17ec1e4c1a9d0eeb866f37982f66dc46a4da393bfe9110313134d0dd160';
@@ -8,7 +8,9 @@ const fetchData = (path) => {
     headers: { Authorization: bearer, Accept: 'application/json' },
   };
 
-  return fetch(`http://phish.in/api/v1/${path}`, optionsObject).then((res) =>
-    res.json()
-  );
+  const res = await fetch(`http://phish.in/api/v1/${path}`, optionsObject);
+  return await res.json();
 };
+
+// return fetch(`http://phish.in/api/v1/${path}`, optionsObject).then((res) =>
+// res.json()
