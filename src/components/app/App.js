d27d591fe4ada32ca96d6182fc9e5b525426cb19
Nina Brissey <nbrissey@gmail.com>
2021-09-13 15:32:24 -0700

    Add ShowContextProvider

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
+import { ShowContext, ShowContextProvider } from '../../contexts/ShowContext';
 
 function App() {
-  return;
-  <div className="App"></div>;
+  return <ShowContextProvider></ShowContextProvider>;
 }
 
 export default App;
