f87ee5ebb16a8de39adf0ae8eacff8be2dffe1ad
Nina Brissey <nbrissey@gmail.com>
2021-09-13 14:31:12 -0700

    Add functions to fetch and pass as value

--- src/contexts/ShowContext.js
+++ src/contexts/ShowContext.js
@@ -4,17 +4,21 @@ import { fetchData } from './apiCalls';
 const ShowContext = createContext();
 
 const ShowContextProvider = (props) => {
-  const [year, setYear] = useState([]);
+  const [years, setYears] = useState([]);
   const [shows, setShows] = useState([]);
   const [currentTrack, setCurrentTrack] = useState({});
   const [isLoading, setIsLoading] = useState(true);
 
   const getShows = (year) => {
-    fetchData(year).then((data) => setShows(data.data));
+    fetchData(`years/${year}`).then((data) => setShows(data.data));
+  };
+
+  const getYears = () => {
+    fetchData('years').then((data) => setYears(data));
   };
 
   return (
-    <ShowContext.Provider value={{ shows, getShows }}>
+    <ShowContext.Provider value={{ year, getYears, shows, getShows }}>
       {props.children}
     </ShowContext.Provider>
   );
