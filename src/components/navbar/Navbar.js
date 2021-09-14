62a2aa168aed9e6d775f019e005d5f27ff63899b
Nina Brissey <nbrissey@gmail.com>
2021-09-14 15:47:20 -0700

    Add functional component for styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,1 +1,7 @@
+import React from 'react';
+
+export default function Navbar() {
+  return <div className="navbar">nav bar</div>;
+}
+
 //  We will conditionally render years drop down menu based on what page we are visiting
