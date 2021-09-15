697868e152fe19339c28755f06dc04840da6ca5e
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-14 18:41:02 -0600

    Add fetch and use context imports

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,6 +1,15 @@
-const Container = ({type}) => {
+import { fetchData } from '../../apiCalls';
+import React, { useContext } from 'react';
+
+
+const Container = () => {
+
+
+ const fetch = () => {fetchData("/years/2021").then(data => console.log(data)).catch(err => console.log(err))}
+ fetch();
+
   return (
-    <p> I am a container </p>
+    <p> I am a container!! </p>
   )
 }
 
