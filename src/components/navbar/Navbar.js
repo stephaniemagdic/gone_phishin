52afc37ba270d43512b56da8ee1c3342c0ba1bef
Nina Brissey <nbrissey@gmail.com>
2021-09-14 22:22:59 -0700

    Add logo image

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import phishInLogo from '../../phish-in-logo.png';
+import './Navbar.css';
 
 export default function Navbar() {
-  return <div className="navbar">nav bar</div>;
+  return (
+    <div className="navbar">
+      <img className="phish-in-logo" src={phishInLogo}></img>
+    </div>
+  );
 }
 
 //  We will conditionally render years drop down menu based on what page we are visiting
