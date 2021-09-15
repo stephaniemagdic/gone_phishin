63fd5545686368294abbda947b432d4e5d9d0c08
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-14 18:51:21 -0600

    Add state to hold showsfrom fetch

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,12 +1,16 @@
 import { fetchData } from '../../apiCalls';
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 
 const Container = (props) => {
-
+const [loading, setIsLoading] = useState(true);
+const [shows, setShows] = useState([]);
 
  useEffect(() => {
-  fetchData(`/years/${props.id}`).then(data => console.log(data)).catch(err => console.log(err))
+  fetchData(`/years/${props.id}`).then(data => {
+    setShows(data);
+    setIsLoading(false);
+  }).catch(err => console.log(err))
   }, [props.id])
 
 
