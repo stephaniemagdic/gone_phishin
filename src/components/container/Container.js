15437f04b6ae50598d0d18916ed3116e0e5b346a
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-14 21:31:42 -0600

    Add fetch for tracks by show to render all tracks

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,11 +1,13 @@
 import { fetchData } from '../../apiCalls';
 import React, { useEffect, useState } from 'react';
 import Show from '../show_card/ShowCard'
-import './Container.css'
+import './Container.css';
+import TrackCard from '../track_card/TrackCard';
 
 const Container = (props) => {
   const [ isLoading, setIsLoading ] = useState(true);
   const [ shows, setShows ] = useState([]);
+  const [ tracks, setTracks ] = useState([])
 
   //TO DO: includes conditional so that we can use this for our tracks and our years. 
   useEffect(() => {
@@ -16,15 +18,24 @@ const Container = (props) => {
       }).catch(err => console.log(err))
       //TO DO: when error component is made, render error component.
     }
-    }, [props.id])
+    if (props.type === "tracks") {
+      fetchData(`/shows/${props.id}`).then(data => {
+        setTracks(data.data.tracks);
+        setIsLoading(false);
+        console.log(data.data)
+      }).catch(err => console.log(err))
+    }
+    }, [])
 
      {/* at some point we should make this conditional along with line 28 depending on what type is passed. */}
     const showCardComponents = shows.map(show => <Show show={show}/>)
+    const trackCardComponents = tracks.map(track => <TrackCard track={track} />)
     
   return (
       <div className={props.type}> 
       {isLoading && <p>Loading...</p>}
       {!isLoading && showCardComponents}
+      {!isLoading && trackCardComponents}
       </div>
   )
 }
