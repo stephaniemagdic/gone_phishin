8c0b2aa32259282e0170626b8db953e3872ae334
Justin Anthony <justincanthony@gmail.com>
2021-09-15 16:26:00 -0600

    Auto format

--- src/components/tracksContainer/TracksContainer.js
+++ src/components/tracksContainer/TracksContainer.js
@@ -1,8 +1,29 @@
 import React, { useState, useEffect, useContext } from 'react';
+import { ShowContext } from '../../contexts/ShowContext';
+
 import './TracksContainer.css';
 
-function TracksContainer() {
-  return <div></div>;
-}
+const TracksContainer = ({ id }) => {
+  const { fetchData } = useContext(ShowContext);
+  const [tracks, setTracks] = useState([]);
+  const [error, setError] = useState('');
+
+  const getTracks = async (id) => {
+    try {
+      const showTracks = await fetchData(`shows/${id}`);
+      setTracks(showTracks.data.tracks);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getTracks(id);
+  }, []);
+
+  const trackCards = tracks.map((track) => <TrackCard track={track} />);
+
+  return <div>{trackCards}</div>;
+};
 
 export default TracksContainer;
