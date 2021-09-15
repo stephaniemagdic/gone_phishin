6f426599cfbcf339e5f3dd095f77bd2668de32ab
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-14 19:47:25 -0600

    Clean up comments and aspacing

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,44 +1,30 @@
 import { fetchData } from '../../apiCalls';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Show from '../show_card/ShowCard'
 
-
 const Container = (props) => {
-const [isLoading, setIsLoading] = useState(true);
-const [shows, setShows] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ shows, setShows ] = useState([]);
 
- useEffect(() => {
-  if(props.type === "shows") {
-    fetchData(`/years/${props.id}`).then(data => {
-      setShows(data.data);
-      setIsLoading(false);
-      console.log(shows)
-      console.log(data)
-    }).catch(err => console.log(err))
-  }
-  }, [props.id])
+  //TO DO: includes conditional so that we can use this for our tracks and our years. 
+  useEffect(() => {
+    if (props.type === "shows") {
+      fetchData(`/years/${props.id}`).then(data => {
+        setShows(data.data);
+        setIsLoading(false);
+        console.log(shows)
+        console.log(data)
+      }).catch(err => console.log(err))
+    }
+    }, [props.id])
 
-//  const updateShows = async () => {
-//    try {
-//     const shows = await fetchData(`/years/${props.id}`);
-//     setShows(shows);
-//     setIsLoading(false)
-//    }
-//    catch {
-    
-//    }
-//  }
-
-  //feels weird that the container is doing the fetch. Feels like it should be in a parent component. 
-
-  const showCardComponents = shows.map(show => <Show show={show}/>)
+    const showCardComponents = shows.map(show => <Show show={show}/>)
 
   return (
     <div className="ShowContainer"> 
       {isLoading && <p>Loading...</p>}
       {!isLoading && showCardComponents}
     </div>
-
   )
 }
 
