b66ddb0c626b6200eeb29dc352954d0f2d431b22
Nina Brissey <nbrissey@gmail.com>
2021-09-15 15:46:46 -0700

    Build basic skeleton

--- src/years_dropdown/YearsDropdown.js
+++ src/years_dropdown/YearsDropdown.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { ShowContext } from '../contexts/ShowContext';
 
 const YearsDropdown = () => {
+  const { years } = useContext(ShowContext);
+
+  const yearsOptions = years.map((year) => {
+    return (
+      <option key={year} value={year}>
+        {year}
+      </option>
+    );
+  });
   return (
     <div>
-      <select>{yearsOptions}</select>
+      <select name="years" id="yearsSelect">
+        <option value="">Years</option>
+        {yearsOptions}
+      </select>
     </div>
   );
 };
