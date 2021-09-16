20b2b523fc634595a1ff06cc0710a2b79221ff2a
Justin Anthony <justincanthony@gmail.com>
2021-09-15 23:15:53 -0600

    Make track clickable to call setCurrentTrack

--- src/components/track_card/TrackCard.js
+++ src/components/track_card/TrackCard.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShowContext } from '../../contexts/ShowContext';
 import './TrackCard.css';
 
 const TrackCard = ({ track }) => {
+  const { getCurrentTrack } = useContext(ShowContext);
   const {
     id,
     mp3,
@@ -12,12 +14,11 @@ const TrackCard = ({ track }) => {
     venue_name,
     waveform_image,
   } = track;
-  console.log(id, 'hello');
 
   return (
-    <article className="track-card">
-      <p>{title}</p>
-      <iframe src={mp3}></iframe>
+    <article className="track-card" id={id} onClick={() => getCurrentTrack(id)}>
+      <p className="song-title">{title}</p>
+      {/* <iframe src={mp3}>{waveform_image}</iframe> */}
     </article>
   );
 };
