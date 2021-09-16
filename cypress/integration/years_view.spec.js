97cac2dd3ee7eb66d24bd704e07c96bed730e67a
Nina Brissey <nbrissey@gmail.com>
2021-09-15 23:15:44 -0700

    Test all elements except click event

--- cypress/integration/years_view.spec.js
+++ cypress/integration/years_view.spec.js
@@ -4,3 +4,26 @@
 // If you're unfamiliar with how Cypress works,
 // check out the link below and learn how to write your first test:
 // https://on.cypress.io/writing-first-test
+
+beforeEach(() => {
+  cy.visit('http://localhost:3000/');
+  cy.fetchYears('http://phish.in/api/v1/years');
+});
+
+describe('Landing Page (Years View)', () => {
+  it('should be able to get all elements on the page', () => {
+    cy.get('img')
+      .should('have.attr', 'src')
+      .should('include', 'phish-in-logo')
+      .get('a')
+      .should('have.attr', 'href')
+      .get('h3')
+      .eq(0)
+      .contains('2021')
+      .get('h3')
+      .eq(29)
+      .contains('1983-1987')
+      .get('footer')
+      .contains('Footer');
+  });
+});
