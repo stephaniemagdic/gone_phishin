2631b6fa57511eb486f5034ea3b547398bbe3912
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-16 08:14:33 -0600

    Add intercept for dropdown user flow test

--- cypress/integration/showDisplay_spec.js
+++ cypress/integration/showDisplay_spec.js
@@ -58,4 +58,52 @@ it('A user should be able to click on a card and be taken to a new page', () => 
   cy.url().should('include', '/shows/2')
 })
 
+it('A user should be able to select a drop down year to see new show data displayed', () => {
+
+  const year = 2020;
+
+    cy.intercept('GET', `http://phish.in/api/v1/years/${year}` , {
+      statusCode: 200,
+      body: { data: [
+        { 
+          date: "DATE TEST 1 FOR 2020",
+          duration: 9658254,
+          id: 1,
+          incomplete: false,
+          likes_count: 5,
+          remastered: false,
+          sbd: false,
+          tags: [],
+          taper_notes: "PhishnBarcelo Maya Grand ResortnRiviera Maya, Quintana Roo, Mexicon02/21/2019nnSource",
+          tour_id: 106,
+          tracks: ['test', 'test', 'test', 'test'],
+          updated_at: "2019-02-28T02:58:30Z",
+          venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name'}, other_names: [{latitude: 20.631389}],
+          venue_name: "TEST NAME 1 FOR 2020"
+        },
+        { 
+          date: "DATE TEST 2 FOR 2020",
+          duration: 9658254,
+          id: 2,
+          incomplete: false,
+          likes_count: 5,
+          remastered: false,
+          sbd: false,
+          tags: [],
+          taper_notes: "Phish\nBarcelo Maya Grand Resort\nRiviera Maya, Quintana Roo, Mexico\n02/21/2019\n\nSource ...",
+          tour_id: 106,
+          tracks: ['test', 'test', 'test', 'test'],
+          updated_at: "2019-02-28T02:58:30Z",
+          venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name2'}, other_names: [{latitude: 20.631389}],
+          venue_name: "TEST NAME 2 FOR 2020"
+        }
+      ]
+      }
+    })
+
+  cy.visit('http://localhost:3000/years/2019')
+  cy.get()
+
+})
+
 });
