54fee56ea9a89bbd9de523674716b61e7323805d
Nina Brissey <nbrissey@gmail.com>
2021-09-16 23:21:18 -0700

    Comment out everything to do with ThemeContext

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import { ShowContextProvider } from '../../contexts/ShowContext';
 import { Route, Switch } from 'react-router-dom';
 import Navbar from '../navbar/Navbar';
 import YearsContainer from '../years_container/YearsContainer';
 import TracksContainer from '../tracksContainer/TracksContainer';
 import Footer from '../footer/Footer';
-import { ShowContext } from '../../contexts/ShowContext';
+// import { ShowContext } from '../../contexts/ShowContext';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ShowsContainer from '../shows_container/ShowsContainer';
+import {
+  ThemeContext,
+  ThemeContextProvider,
+} from '../../contexts/ThemeContext';
 
 const App = () => {
   //To Do
@@ -22,48 +26,55 @@ const App = () => {
   //   }
   // }, [error]);
 
+  // const { isPartyMode, partyMode, basic } = useContext(ThemeContext);
+  // const themes = isPartyMode ? partyMode : basic;
+
   return (
-    <ShowContextProvider>
-      <Navbar />
-      <Switch>
-        {/* {loading && <Loading />} */}
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return (
-              <div className="years-view">
-                <YearsContainer />
-              </div>
-            );
-          }}
-        />
-        <Route
-          exact
-          path="/years/:year"
-          render={({ match }) => {
-            return (
-              <div className="app">
-                <ShowsContainer year={match.params.year} />
-              </div>
-            );
-          }}
-        />
-        <Route
-          exact
-          path="/shows/:id"
-          render={({ match }) => {
-            return (
-              <div className="app">
-                <TracksContainer id={match.params.id} type="tracks" />
-              </div>
-            );
-          }}
-        />
-        <Route render={() => <p>Error. Page Not Found!</p>} />
-      </Switch>
-      <Footer />
-    </ShowContextProvider>
+    <ThemeContextProvider>
+      <ShowContextProvider>
+        {/* <div style={{ backgroundColor: themes.mainBG }}> */}
+        <Navbar />
+        <Switch>
+          {/* {loading && <Loading />} */}
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <div className="years-view">
+                  <YearsContainer />
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/years/:year"
+            render={({ match }) => {
+              return (
+                <div className="app">
+                  <ShowsContainer year={match.params.year} />
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/shows/:id"
+            render={({ match }) => {
+              return (
+                <div className="app">
+                  <TracksContainer id={match.params.id} type="tracks" />
+                </div>
+              );
+            }}
+          />
+          <Route render={() => <p>Error. Page Not Found!</p>} />
+        </Switch>
+        <Footer />
+        {/* </div> */}
+      </ShowContextProvider>
+    </ThemeContextProvider>
   );
 };
 
