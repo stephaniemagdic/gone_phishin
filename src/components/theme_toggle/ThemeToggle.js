1d380f7b520ff35a179c15fa526642be2ab1002e
Nina Brissey <nbrissey@gmail.com>
2021-09-16 22:23:02 -0700

    Build ToggleTheme logic

--- src/components/theme_toggle/ThemeToggle.js
+++ src/components/theme_toggle/ThemeToggle.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../../contexts/ThemeContext';
 
 const ThemeToggle = () => {
-  return <div></div>;
+  const { toggleTheme } = useContext(ThemeContext);
+  const { isPartyMode, partyMode, basic } = useContext(ThemeContext);
+  const theme = isPartyMode ? partyMode : basic;
+  return (
+    <button onClick={toggleTheme}>
+      {theme.isPartyMode ? 'Party Mode' : 'Standard View'}
+    </button>
+  );
 };
 
 export default ThemeToggle;
