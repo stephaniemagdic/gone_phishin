6af9d11114d910206bb38848a4f09bb862be7376
Nina Brissey <nbrissey@gmail.com>
2021-09-17 15:33:53 -0700

    Add inline styling using theme context

--- src/components/year_card/YearCard.js
+++ src/components/year_card/YearCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useTheme } from '../../contexts/ThemeContext';
 import './YearCard.css';
 
 const YearCard = ({ year }) => {
+  // const { years } = useContext(ShowContext);
+
+  const { isPartyMode, partyMode, basic } = useTheme();
+  const theme = isPartyMode ? partyMode : basic;
+
   return (
-    <div className="year-card" id={year}>
-      <h3 className="year-text">{year}</h3>
+    <div style={{ background: theme.mainBG }} className="year-card" id={year}>
+      <h3 style={{ color: theme.text }} className="year-text">
+        {year}
+      </h3>
     </div>
   );
 };
