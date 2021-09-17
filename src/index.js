8d66e5357a5c4bca8f198c6c269b21a805504379
Nina Brissey <nbrissey@gmail.com>
2021-09-16 21:59:06 -0700

    Import ThemeContext, ThemeContextProvider, useContext

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeContext, ThemeContextProvider } from './contexts/ThemeContext';
 
+const { isPartyMode, partyMode, basic } = useContext(ThemeContext);
+const theme = isPartyMode ? partyMode : basic;
 
 ReactDOM.render(
   <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ThemeContextProvider>
+      <React.StrictMode>
+        <div style={{ backgroundColor: theme.mainBG }}>
+          <App />
+        </div>
+      </React.StrictMode>
+    </ThemeContextProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
