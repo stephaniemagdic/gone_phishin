6c4a7f0c6afbb8e056785fab37896360d371b240
Nina Brissey <nbrissey@gmail.com>
2021-09-16 22:22:35 -0700

    Add ThemeToggle component

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import YearsDropdown from '../years_dropdown/YearsDropdown';
 import phishInLogo from '../../phish-in-logo.png';
+import ThemeToggle from '../theme_toggle/ThemeToggle';
 import './Navbar.css';
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <img className="phish-in-logo" src={phishInLogo}></img>
+      <ThemeToggle />
       <YearsDropdown />
     </div>
   );
