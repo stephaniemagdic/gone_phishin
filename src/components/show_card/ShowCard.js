2e79ab0687136cd301c0a7792f74951dd8326505
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-18 08:03:34 -0600

    Add party mode state through Theme Context

--- src/components/show_card/ShowCard.js
+++ src/components/show_card/ShowCard.js
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
 import './ShowCard.css';
+import { useTheme } from '../../contexts/ThemeContext';
+
 
 const ShowCard = ({ show }) => {
+  const { isPartyMode, partyMode, basic } = useTheme();
+  const theme = isPartyMode ? partyMode : basic;
   const { date, venue_name, id } = show;
   return (
     <Link to={`/shows/${id}`} className="link">
-      <article className="show-card">
+      <article className="show-card" >
         <p> {date} </p>
         <p> {venue_name} </p>
       </article>
