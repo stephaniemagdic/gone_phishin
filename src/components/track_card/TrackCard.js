3ff1303834655189d100772790fd09b07b1b6d1b
Justin Anthony <justincanthony@gmail.com>
2021-09-18 13:59:54 -0600

    Remove unused variables

--- src/components/track_card/TrackCard.js
+++ src/components/track_card/TrackCard.js
@@ -8,21 +8,11 @@ const TrackCard = ({ track }) => {
   const { getCurrentTrack } = useContext(ShowContext);
   const { isPartyMode, partyMode, basic } = useTheme();
   const theme = isPartyMode ? partyMode : basic;
-  const {
-    id,
-    // mp3,
-    // show_date,
-    // show_id,
-    title,
-    // venue_location,
-    // venue_name,
-    // waveform_image,
-  } = track;
+  const { id, title } = track;
 
   return (
     <article className="card" id={id} onClick={() => getCurrentTrack(id)}>
       <p className="song-title">{title}</p>
-      {/* <iframe src={mp3}>{waveform_image}</iframe> */}
     </article>
   );
 };
