274f04a1f81a3be66caec827f97daadd24f7dcc2
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-18 12:34:05 -0600

    Clean up comments and spacing

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -56,7 +56,7 @@ Cypress.Commands.add('fetchYears', (url) => {
   });
 });
 
-// Commands for showDisplay 
+//  SHOW DISPLAY COMMANDS
 Cypress.Commands.add('fetch2019Shows', () => {
   cy.intercept('GET', 'http://phish.in/api/v1/years/2019' , {
       statusCode: 200,
@@ -115,7 +115,8 @@ Cypress.Commands.add('fetch2020Shows', () => {
       tour_id: 106,
       tracks: ['test', 'test', 'test', 'test'],
       updated_at: "2019-02-28T02:58:30Z",
-      venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name'}, other_names: [{latitude: 20.631389}],
+      venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name', location: 'TEST LOCATION 1 FOR 2020'}, 
+      other_names: [{latitude: 20.631389}],
       venue_name: "TEST NAME 1 FOR 2020"
     },
     { 
@@ -131,12 +132,41 @@ Cypress.Commands.add('fetch2020Shows', () => {
       tour_id: 106,
       tracks: ['test', 'test', 'test', 'test'],
       updated_at: "2019-02-28T02:58:30Z",
-      venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name2'}, other_names: [{latitude: 20.631389}],
+      venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name2', location: 'TEST LOCATION 2 FOR 2020'}, 
+      other_names: [{latitude: 20.631389}],
       venue_name: "TEST NAME 2 FOR 2020"
     }
   ]
   }
+  })
 })
+
+Cypress.Commands.add('fetchShowData', () => {
+  cy.intercept('GET', 'http://phish.in/api/v1/shows/2' , {
+  statusCode: 200,
+  body: { 
+    data: { 
+      date: "TEST SHOW DATE",
+      duration: 9658254,
+      id: 2,
+      incomplete: false,
+      likes_count: 5,
+      remastered: false,
+      sbd: false,
+      tags: [],
+      taper_notes: "Phish\nBarcelo Maya Grand Resort\nRiviera Maya, Quintana Roo, Mexico\n02/21/2019\n\nSource ...",
+      tour_id: 106,
+      tracks: ['test', 'test', 'test', 'test'],
+      updated_at: "2019-02-28T02:58:30Z",
+      venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name2', location: 'TEST LOCATION 2'}, other_names: [{latitude: 20.631389}],
+      venue_name: "TEST NAME 2"
+    }
+  }
+})
+})
+
+
+
 //
 //
 // -- This is a child command --
