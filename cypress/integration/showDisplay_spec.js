d4b8491be531ef643e18a2c9c775f3f02b2bbf39
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-18 12:16:54 -0600

    Move command stubs to start of it blocks to fix bug

--- cypress/integration/showDisplay_spec.js
+++ cypress/integration/showDisplay_spec.js
@@ -13,19 +13,35 @@ describe('Show Display User Flows', () => {
 
 
   it('A user should be able to click on a card and be taken to a new page', () => {
+    cy.fetchShowData() 
     cy.get('.show-card').first().click()
     cy.url().should('include', '/shows/1')
     cy.fetch2019Shows()
     cy.visit('http://localhost:3000/years/2019')
     cy.get('.show-card').last().click()
     cy.url().should('include', '/shows/2')
+    //HERE THE INTERCEPT IS NOT WORKING.
+   
+    // cy.fetchShowData() 
+    cy.get('.show-info')
+    cy.contains('TEST SHOW DATE')
   })
 
 it('A user should be able to select a drop down year to see new show data displayed', () => {
 
-
-  // cy.visit('http://localhost:3000/years/2019')
-  // cy.get('select').select('2020')
+  cy.fetch2020Shows()
+  cy.visit('http://localhost:3000/years/2019')
+  cy.get('select').select('2020')
+  // cy.route('http://localhost:3000/years/2019').wait()
+  // cy.fetch2020Shows()
+  // cy.request('http://localhost:3000/years/2020')
+  // cy.url().should('include', 2020)
+  // cy.get('.show-card').first().get('.venue').contains('DATE TEST 1 FOR 2020')
+  // cy.get('.date').contains('DATE TEST 1')
+  // cy.get('.location').contains('TEST LOCATION 1')
+  // cy.get('.show-card').last().get('.venue').contains('TEST NAME 2')
+  // cy.get('.date').contains('DATE TEST 2')
+  // cy.get('.location').contains('TEST LOCATION 2')
   //there is some sort of interruption that happens. 
   //it loads and then I think a fetch then intercepts once more.
   //I think we need to change the route -- reroute as well when we update the movies on the page. .. so instead we could create a redirect.
