5d8f80ee442707a7b7c0fb11db8c33362a9aff20
Nina Brissey <nbrissey@gmail.com>
2021-09-17 18:19:11 -0700

    Add styling

--- src/components/year_card/YearCard.js
+++ src/components/year_card/YearCard.js
@@ -9,8 +9,19 @@ const YearCard = ({ year }) => {
   const theme = isPartyMode ? partyMode : basic;
 
   return (
-    <div style={{ background: theme.mainBG }} className="year-card" id={year}>
-      <h3 style={{ color: theme.text }} className="year-text">
+    <div
+      style={{
+        background: theme.primaryBG,
+        boxShadow: theme.shadow,
+        borderRadius: theme.radius,
+      }}
+      className="year-card"
+      id={year}
+    >
+      <h3
+        style={{ color: theme.primaryText, background: theme.secondaryBG }}
+        className="year-text"
+      >
         {year}
       </h3>
     </div>
