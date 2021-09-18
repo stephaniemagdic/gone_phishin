ff78dc196536ea3e7151ba95902795123ffd51ac
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-18 08:45:41 -0600

    Create fetch by year command and move stub

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -56,6 +56,48 @@ Cypress.Commands.add('fetchYears', (url) => {
   });
 });
 
+// Commands for showDisplay 
+Cypress.commands.add('fetch2019Shows', (year) => {
+  
+  cy.intercept('GET', `http://phish.in/api/v1/years/${year}` , {
+      statusCode: 200,
+      body: { data: [
+        { 
+          date: "DATE TEST 1",
+          duration: 9658254,
+          id: 1,
+          incomplete: false,
+          likes_count: 5,
+          remastered: false,
+          sbd: false,
+          tags: [],
+          taper_notes: "PhishnBarcelo Maya Grand ResortnRiviera Maya, Quintana Roo, Mexicon02/21/2019nnSource",
+          tour_id: 106,
+          tracks: ['test', 'test', 'test', 'test'],
+          updated_at: "2019-02-28T02:58:30Z",
+          venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name'}, other_names: [{latitude: 20.631389}],
+          venue_name: "TEST NAME 1"
+        },
+        { 
+          date: "DATE TEST 2",
+          duration: 9658254,
+          id: 2,
+          incomplete: false,
+          likes_count: 5,
+          remastered: false,
+          sbd: false,
+          tags: [],
+          taper_notes: "Phish\nBarcelo Maya Grand Resort\nRiviera Maya, Quintana Roo, Mexico\n02/21/2019\n\nSource ...",
+          tour_id: 106,
+          tracks: ['test', 'test', 'test', 'test'],
+          updated_at: "2019-02-28T02:58:30Z",
+          venue: {id: 817, slug: 'barcelo-maya-beach', name: 'name2'}, other_names: [{latitude: 20.631389}],
+          venue_name: "TEST NAME 2"
+        }
+      ]
+      }
+    }
+  )
 //
 //
 // -- This is a child command --
