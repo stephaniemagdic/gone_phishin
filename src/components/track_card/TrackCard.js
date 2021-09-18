2c238c8ea104525fc7fd2adc7cbd26f02c913490
Justin Anthony <justincanthony@gmail.com>
2021-09-18 13:12:06 -0600

    Import theme context to trackscard

--- src/components/track_card/TrackCard.js
+++ src/components/track_card/TrackCard.js
@@ -1,9 +1,13 @@
 import React, { useContext } from 'react';
 import { ShowContext } from '../../contexts/ShowContext';
+import { useTheme } from '../../contexts/ThemeContext';
+
 import './TrackCard.css';
 
 const TrackCard = ({ track }) => {
   const { getCurrentTrack } = useContext(ShowContext);
+  const { isPartyMode, partyMode, basic } = useTheme();
+  const theme = isPartyMode ? partyMode : basic;
   const {
     id,
     // mp3,
