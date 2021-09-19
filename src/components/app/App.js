40b4247f408e7932ab2be9277a91af40fe280379
Justin Anthony <justincanthony@gmail.com>
2021-09-19 17:51:44 -0600

    Render error for bad path

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -50,7 +50,7 @@ const App = () => {
                 );
               }}
             />
-            <Route render={() => <ErrorDisplay />} />
+            <Route render={() => <ErrorDisplay message={'Page Not Found'} />} />
           </Switch>
           <Footer />
         </div>
