642277a697ee6b5ad0a3fe6198c07f2edca61deb
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-20 14:09:32 -0600

    Add image imports

--- src/components/error_display/ErrorDisplay.js
+++ src/components/error_display/ErrorDisplay.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './ErrorDisplay.css';
+import underConstruction from '../../error.png';
+import underConstructionParty from '../../error_party.png';
+import { useTheme } from '../../contexts/ThemeContext';
 
 const ErrorDisplay = ({ message }) => {
-  return <p>{message}</p>;
+  const { isPartyMode } = useTheme();
+  const errorImage = isPartyMode ? underConstructionParty : underConstruction;
+
+  return (
+    <div className='error-display'>
+    </div>);
 };
 
 export default ErrorDisplay;
