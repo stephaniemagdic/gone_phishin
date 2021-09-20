724f907b96667db01f709af598410c3ed9536a03
Nina Brissey <nbrissey@gmail.com>
2021-09-20 08:43:12 -0700

    Add isPartyMode context to manipulate mode icons

--- src/components/theme_toggle/ThemeToggle.js
+++ src/components/theme_toggle/ThemeToggle.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { useTheme } from '../../contexts/ThemeContext';
+import partyModeIcon from '../../party-mode-icon.png';
+import standardModeIcon from '../../standard-mode-icon.png';
+import './ThemeToggle.css';
 
 const ThemeToggle = () => {
-  const { toggleTheme } = useTheme();
-  return <button onClick={toggleTheme}>Change Theme</button>;
+  const { isPartyMode, toggleTheme } = useTheme();
+  // return <button onClick={toggleTheme}>Change Theme</button>;
+  return (
+    // <div></div>
+    <img
+      className="mode-icon"
+      src={isPartyMode ? partyModeIcon : standardModeIcon}
+      onClick={toggleTheme}
+    />
+  );
 };
 
 export default ThemeToggle;
