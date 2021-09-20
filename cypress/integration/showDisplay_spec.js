a4b058fa3d4296a63e1b202c9ab3af2df80023aa
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-20 14:40:58 -0600

    Add new className to fix bug

--- cypress/integration/showDisplay_spec.js
+++ cypress/integration/showDisplay_spec.js
@@ -3,10 +3,10 @@ describe('Show Display User Flows', () => {
     cy.fetch2019Shows() 
     cy.visit('http://localhost:3000/years/2019')
     cy.url().should('include', 2019)
-    cy.get('.show-card').first().get('.venue').contains('TEST NAME 1')
+    cy.get('.card').first().get('.venue').contains('TEST NAME 1')
     cy.get('.date').contains('DATE TEST 1')
     cy.get('.location').contains('TEST LOCATION 1')
-    cy.get('.show-card').last().get('.venue').contains('TEST NAME 2')
+    cy.get('.card').last().get('.venue').contains('TEST NAME 2')
     cy.get('.date').contains('DATE TEST 2')
     cy.get('.location').contains('TEST LOCATION 2')
   });
@@ -14,11 +14,11 @@ describe('Show Display User Flows', () => {
 
   it('A user should be able to click on a card and be taken to a new page', () => {
     cy.fetchShowData() 
-    cy.get('.show-card').first().click()
+    cy.get('.card').first().click()
     cy.url().should('include', '/shows/1')
     cy.fetch2019Shows()
     cy.visit('http://localhost:3000/years/2019')
-    cy.get('.show-card').last().click()
+    cy.get('.card').last().click()
     cy.url().should('include', '/shows/2')
     cy.get('.show-info')
     cy.contains('TEST SHOW DATE')
@@ -31,16 +31,16 @@ describe('Show Display User Flows', () => {
     cy.visit('http://localhost:3000/years/2019')
     cy.get('select').select('2020')
     cy.url().should('include', 2020)
-    cy.get('.show-card').first().get('.date').contains('DATE TEST 1 FOR 2020')
+    cy.get('.card').first().get('.date').contains('DATE TEST 1 FOR 2020')
     cy.get('.location').contains('TEST LOCATION 1 FOR 2020')
-    cy.get('.show-card').last().get('.venue').contains('TEST NAME 1 FOR 2020')
+    cy.get('.card').last().get('.venue').contains('TEST NAME 1 FOR 2020')
   })
 
   //START OF TESTING PARTY MODE COLOR
   // it('A user should be able to click the Change Theme button and see party mode styling', () => {
   //   cy.visit('http://localhost:3000/years/2019')
   //   cy.get('button').click()
-  //    cy.get('.show-card').should('have.css', 'color', '#A81AE5')
+  //    cy.get('.card').should('have.css', 'color', '#A81AE5')
 
   // })
 
