1aba36f303849efabd66e9e7a5a64a1b87326906
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-20 16:27:42 -0600

    Change paths

--- cypress/integration/errorDisplay_spec.js
+++ cypress/integration/errorDisplay_spec.js
@@ -1,6 +1,6 @@
 describe('Show Display User Flows', () => {
   it('A user should be taken to an error page if bad path is visited', () => {
-    cy.visit('http://localhost:3000/years/idontexist')
+    cy.visit('years/idontexist')
     cy.get('.error-display')
   })
 
