5fc7043216358db13103172d005f04213e780869
Justin Anthony <justincanthony@gmail.com>
2021-09-19 18:34:08 -0600

    Remove commented out code

--- src/components/years_container/YearsContainer.js
+++ src/components/years_container/YearsContainer.js
@@ -25,12 +25,3 @@ const YearsContainer = () => {
 };
 
 export default YearsContainer;
-
-// const { years, setYears, getData } = useContext(ShowContext);
-
-// getData([props.path], [props.setData]).catch((err) => console.log(err));
-
-// const cards = [props.type].map((item) => {
-//   <props.card key={item.id} item={item} />;
-// });
-// return <div>{cards}</div>;
