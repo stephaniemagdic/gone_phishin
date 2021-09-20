e0ff0ce2b7f0f07c356f8d6bcb5d49c69404e27d
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-20 15:40:32 -0600

    Add test to check that logo takes user to main page

--- cypress/integration/errorDisplay_spec.js
+++ cypress/integration/errorDisplay_spec.js
@@ -1,9 +1,12 @@
 describe('Show Display User Flows', () => {
-it('A user should be taken to an error page if the server is down', () => {
-  // cy.makeRequestWithServerDown()
-  cy.visit('http://localhost:3000/years/idontexist')
-  cy.get('.error-display')
-})
+  it('A user should be taken to an error page if the server is down', () => {
+    cy.visit('http://localhost:3000/years/idontexist')
+    cy.get('.error-display')
+  })
 
+  it('Phish in page logo should take user back to the home page', () => {
+    cy.get('.phish-in-logo').click()
+    cy.url().should('not.include', 'idontexist')
+  })
 
 })
