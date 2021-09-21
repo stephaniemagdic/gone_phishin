70213ee8e0c5a24ac861ebd254b06575e5926298
Nina Brissey <nbrissey@gmail.com>
2021-09-20 21:30:18 -0700

    Format date using dayjs, change classnames and grouping

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,22 +3,29 @@ import ReactAudioPlayer from 'react-audio-player';
 import './Footer.css';
 import { useContext } from 'react';
 import { ShowContext } from '../../contexts/ShowContext';
+import dayjs from 'dayjs';
 
 export default function Footer() {
   const { currentTrack } = useContext(ShowContext);
   const { mp3, title, show_date, venue_name, venue_location } = currentTrack;
+  const formattedDate = dayjs(show_date).format('M-D-YYYY');
 
   return (
     <footer className="footer">
-      <div className="track-info">
-        <p>{title}</p>
-      </div>
-      <div className="venue-info">
-        <p className="venue-text">{show_date}</p>
-        <p className="venue-text">{venue_name}</p>
-        <p className="venue-text">{venue_location}</p>
-      </div>
-      <ReactAudioPlayer src={mp3} autoPlay={true} controls={true} />
+      {/* <div className="track-info"> */}
+      <p className="venue-text venue-title">{title}</p>
+      {/* </div> */}
+      {/* <div className="venue-info"> */}
+      <p className="venue-text">{formattedDate}</p>
+      <p className="venue-text">{venue_name}</p>
+      <p className="venue-text">{venue_location}</p>
+      {/* </div> */}
+      <ReactAudioPlayer
+        className="audio-player"
+        src={mp3}
+        autoPlay={true}
+        controls={true}
+      />
     </footer>
   );
 }
