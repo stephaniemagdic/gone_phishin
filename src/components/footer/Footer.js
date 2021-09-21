01422e5c8c82ca24b39bbca5cdfde964930d40ab
Nina Brissey <nbrissey@gmail.com>
2021-09-20 21:53:13 -0700

    Add theme context

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,6 +3,7 @@ import ReactAudioPlayer from 'react-audio-player';
 import './Footer.css';
 import { useContext } from 'react';
 import { ShowContext } from '../../contexts/ShowContext';
+import { useTheme } from '../../contexts/ThemeContext';
 import dayjs from 'dayjs';
 
 export default function Footer() {
@@ -10,12 +11,26 @@ export default function Footer() {
   const { mp3, title, show_date, venue_name, venue_location } = currentTrack;
   const formattedDate = dayjs(show_date).format('M-D-YYYY');
 
+  const { isPartyMode, partyMode, basic } = useTheme();
+  const theme = isPartyMode ? partyMode : basic;
+
   return (
-    <footer className="footer">
-      <p className="venue-text venue-title">{title}</p>
-      <p className="venue-text">{formattedDate}</p>
-      <p className="venue-text">{venue_name}</p>
-      <p className="venue-text">{venue_location}</p>
+    <footer className="footer" style={{ background: theme.aquaGradient }}>
+      <p
+        className="venue-text venue-title"
+        style={{ color: theme.primaryText }}
+      >
+        {title}
+      </p>
+      <p className="venue-text" style={{ color: theme.primaryText }}>
+        {formattedDate}
+      </p>
+      <p className="venue-text" style={{ color: theme.primaryText }}>
+        {venue_name}
+      </p>
+      <p className="venue-text" style={{ color: theme.primaryText }}>
+        {venue_location}
+      </p>
       <ReactAudioPlayer
         className="audio-player"
         src={mp3}
