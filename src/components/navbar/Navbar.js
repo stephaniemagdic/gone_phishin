8949aca892e9a9a17ebd2c661e847dba6cd0e9cc
Stephanie Magdic <stephaniemagdic@gmail.com>
2021-09-29 19:48:51 -0600

    Fix bug

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
       <div className="mode">
         <ThemeToggle />
       </div>
-      <Link className="phish-in-link" to="/">
+      <Link className="phish-in-link" to="/gone_phishin/">
         <img
           className="phish-in-logo"
           alt="phish-in-logo"
